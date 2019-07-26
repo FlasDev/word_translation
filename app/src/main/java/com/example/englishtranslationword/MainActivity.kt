5978c942e13abb746d1f8285b2e70bc30785f16d
Oleh Chausov <oleh_chausov@epam.com>
2019-07-26 16:35:19 +0300

    viewpager added

--- app/src/main/java/com/example/englishtranslationword/MainActivity.kt
+++ app/src/main/java/com/example/englishtranslationword/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.englishtranslationword
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.englishtranslationword.adapters.ViewPagerStateAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,5 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewPager.adapter = ViewPagerStateAdapter(this)
+
+
+        NavigationUI.setupWithNavController(bottomNavigationView, bottomNavigationView
+            .findNavController())
     }
 }
